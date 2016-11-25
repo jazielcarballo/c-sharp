b4a3c07be9c72a63272883647f409c6b09728e19
Jaziel Aarón Carballo Tadeo <jaziel.carballo@gmail.com>
2016-11-25 08:12:23 -0600

    Add files via upload
    Ejercicio 5 del siguiente problema: 
    Hacer un programa creando una función que: 
    1. Pida cantidad de libros a pagar 
    2. El costo de cada libro es de $80 
    3. Si son más de 10 libros se hace un descuento del 10% sobre el pago total. 
    4. Imprimir pago total

---
+++ ejercicio5/ejercicio5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ejercicio5
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// Variables necesarias
+			string libros = "";
+			float cantLibros = 0;
+			double resultado = 0.0f;
+
+			Console.WriteLine("<<<<<Librería>>>>>");
+			Console.WriteLine("¿Cuántos libros?");
+			libros = Console.ReadLine();
+			cantLibros = Convert.ToSingle(libros);
+
+			resultado = operacion(cantLibros);
+
+			Console.WriteLine("El resultado es {0}",resultado);
+		}
+
+		static double operacion(float libros)
+		{
+			//Variables total
+			float totalParcial = 0;
+			double total = 0;
+
+			//Variable descuento
+			double descuento = 0;
+
+			//Constante precio libro
+			float precio = 80;
+
+			//Multiplicamos el costo de cada libro por su total
+			totalParcial = precio * libros;
+			//Verificamos si son más de 10 libros
+
+			if (libros > 10)
+			{
+				descuento = totalParcial * .10;
+				total = totalParcial - descuento;
+			}
+
+			else 
+			{
+				total = totalParcial;
+			}
+
+			return total;
+
+		}
+	}
+}
