f583866bec53015932fa61daccdd94c2bda0b70f
Jaziel Aarón Carballo Tadeo <jaziel.carballo@gmail.com>
2016-11-28 07:36:59 -0600

    Add files via upload
    Programa para calcular la edad

---
+++ edad/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace edad
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//Declaro e inicializo variables 
+			string anio = "";
+			float anioNac = 0;
+			float edadFinal = 0;
+
+			Console.WriteLine("Calcular edad");
+			//Pregunto año de nacimiento
+			Console.WriteLine("Año de nacimiento");
+			anio = Console.ReadLine();
+			anioNac = Convert.ToSingle(anio);
+
+			//Llamo al método
+			edadFinal = calcularEdad(anioNac);
+			//Imprimir resultado
+			Console.WriteLine("Tu edad es {0}", edadFinal);
+		}
+
+		static float calcularEdad(float anioNac)
+		{
+			float edad = 0;
+			float anioActual = 2016;
+			edad = anioActual - anioNac;
+			return edad;
+		}
+	}
+}
