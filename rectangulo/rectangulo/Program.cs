220d00bc481d3cbe452f022e14bdc3c0ecb11e4a
Jaziel Aarón Carballo Tadeo <jaziel.carballo@gmail.com>
2016-11-30 10:25:21 -0600

    Add files via upload
    Para ver el código ir a:
    /rectangulo/rectangulo/Program.cs

---
+++ rectangulo/rectangulo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ejercicio7
+{
+	
+
+	class rectangulo
+	{
+		public int b;
+		public int h;
+		public int area;
+
+		public void CalcularAreaRect()
+		{
+			area = b * h;
+		}
+
+	}
+
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			
+			//Instanciar a la clase rectangulo
+			rectangulo miRectangulo = new rectangulo();
+
+			//Asignar valor a base y altura
+
+			miRectangulo.b = 8;
+			miRectangulo.h = 4;
+
+	
+			//Invocar el metodo
+			miRectangulo.CalcularAreaRect();
+
+			// Desplegamos los datos
+		
+			Console.WriteLine("El área del rectángulo es ={0}", miRectangulo.area);
+
+
+
+
+		}
+	}
+}
